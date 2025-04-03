bb78460fcb5a5cda53547d3ccd3b0e0addd5a3aa
michele55 <russoantoniomichele9@gmail.com>
2025-04-03 17:12:33 +0200

    Variazioni testing e frontend

--- Frontend/cypress/cypress.config.js
+++ Frontend/cypress/cypress.config.js
@@ -1,6 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: false},
   e2e: {
     specPattern: 'e2e/*.{cy,cy.js,js,ts}',
     supportFile: 'support/e2e.js', 
