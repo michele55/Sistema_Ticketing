b09fee1e09f68dce8757c140486e659b5290b3d6
michele55 <russoantoniomichele9@gmail.com>
2025-04-08 01:21:35 +0200

    mocha

--- Frontend/cypress/e2e/specNewTicket.cy.js
+++ Frontend/cypress/e2e/specNewTicket.cy.js
@@ -2,28 +2,20 @@ describe('Creazione Nuovo Ticket da parte del Customer', () => {
     it('Login come customer, clic su NUOVO TICKET e invio ticket', () => {
       // 1. Visita la pagina di login
       cy.visit('http://localhost:9000/#/');
-  
       // 2. Login come customer
       cy.get('input[name="Email"]').type('customer@example.com');
       cy.get('input[name="Password"]').type('password');
       cy.get('button[type="submit"]').click();
-  
       // 3. Verifica che sia nella dashboard
       cy.url().should('include', '/Dashboard');
-  
       // 4. Clic su "NUOVO TICKET"
       cy.get('.q-tab').contains('Nuovo Ticket').click();
-
-  
       // 5. Inserisce titolo e descrizione
       const titolo = 'Bug test Cypress ' + Date.now();
       cy.get('[data-cy="input-titolo"]').type(titolo);
       cy.get('[data-cy="input-descrizione"]').type('Questo ticket è stato creato automaticamente durante un test.');
-  
       // Invia
       cy.get('[data-cy="btn-invia"]').click();
-  
-   
       cy.get('.q-notification')
       .should('be.visible')
       .and('contain', 'Ticket creato con successo!');
